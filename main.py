c8e7a9e3cfaf7b1b128c52f16312908c222fbc8f
vijdan-anjum <m-vijdan@hotmail.com>
2024-12-04 20:28:21 -0600

    restricted send_gossip arg updated to None

--- main.py
+++ main.py
@@ -8,17 +8,10 @@ def main():
     my_peer = Peer(8993, "u-neeq name")
     event_q = EventQueue()
 
-    # Known university peers
-    UNI_PEERS = [
-        ["silicon.cs.umanitoba.ca", 8999],  # website is down
-        ["eagle.cs.umanitoba.ca", 8999],
-        ["hawk.cs.umanitoba.ca", 8999]
-    ]
-
     # Start listening in a separate thread
     threading.Thread(target=my_peer.listen, daemon=True).start()
 
-    event_q.add_event(time.time() + 1, my_peer.send_gossip, UNI_PEERS[1], 30)
+    event_q.add_event(time.time() + 1, my_peer.send_gossip, None, 30)
     # event_q.add_event(time.time() + 20, my_peer.send_stats, [my_peer.received_gossipers], 30)
 
     while True:
