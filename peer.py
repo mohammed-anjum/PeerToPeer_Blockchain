b1fef7ce42315c786c076c37bc1b8ea04c0ca586
vijdan-anjum <m-vijdan@hotmail.com>
2024-12-04 20:24:43 -0600

    restricted send_gossip to UNI PEERS and only eagle for now. added print messages for gossip testing

--- peer.py
+++ peer.py
@@ -2,6 +2,7 @@ import socket
 import json
 import uuid
 
+
 class Peer:
     def __init__(self, port, name):
         # Automatically pick up the current IP
@@ -12,6 +13,14 @@ class Peer:
         self.socket.bind((self.host, self.port))
         self.received_gossipers = {}
         self.received_stats = {}
+        self.uni_peer = [] # we will implement this later
+        self.GOSSIP_MSG = {
+            "type": "GOSSIP",
+            "host": self.host,
+            "port": self.port,
+            "id": str(uuid.uuid4()),
+            "name": self.name
+        }
         ### DO NOT REMOVE THIS PRINT
         print(f"Peer started at {self.host}:{self.port}, The name: {name}")
 
@@ -24,8 +33,7 @@ class Peer:
         host, port, msg_type, message = validate_msg(addr, msg)
 
         if msg_type == "GOSSIP":
-            # print(f"--GOSSIP--\n\t{addr}: {message}\n")
-            pass
+            self.send_gossip_reply(host, port)
 
         elif msg_type == "GOSSIP_REPLY":
             # print(f"--GOSSIP_REPLY--\n\t{addr}: {message}\n")
@@ -50,19 +58,36 @@ class Peer:
         else:
             print(f"--UNKNOWN--\n\t{addr}: {message}\n")
 
-    def send_gossip(self, target_host, target_port):
-        """Send a simple gossip msg."""
-        # print(f"SENDING GOSSIP to {target_host}:{target_port}")
+    def send_gossip(self):
+        """Send a simple gossip msg TO UNI PEERS"""
+
+        UNI_PEERS = [
+            ["silicon.cs.umanitoba.ca", 8999],  # website is down
+            ["eagle.cs.umanitoba.ca", 8999],
+            ["hawk.cs.umanitoba.ca", 8999],
+            ["grebe.cs.umanitoba.ca", 8999],
+            ["goose.cs.umanitoba.ca", 8999]
+        ]
+
+        # for now i have defaulted to eagle only
+        print(f"SENDING GOSSIP to {UNI_PEERS[1][0]}:{UNI_PEERS[1][1]}")
+
+        data = json.dumps(self.GOSSIP_MSG).encode('utf-8')
+        self.socket.sendto(data, (UNI_PEERS[1][0], UNI_PEERS[1][1]))
+        print(f"--GOSSIP_SENT--\n\tto {UNI_PEERS[1][0]}:{UNI_PEERS[1][1]}\n")
+
+    def send_gossip_reply(self, target_host, target_port):
+        """Send a gossip reply to FROM peer"""
+        print(f"SENDING GOSSIP_REPLY to {target_host}:{target_port}")
         msg = {
-            "type": "GOSSIP",
+            "type": "GOSSIP_REPLY",
             "host": self.host,
             "port": self.port,
-            "id": str(uuid.uuid4()),
             "name": self.name
         }
         data = json.dumps(msg).encode('utf-8')
         self.socket.sendto(data, (target_host, target_port))
-        # print(f"--GOSSIP_SENT--\n\tto {target_host}:{target_port}\n")
+        print(f"--GOSSIP_REPLY_SENT--\n\tto {target_host}:{target_port}\n")
 
     def send_stats(self, target_list):
         if len(target_list) != 0:
@@ -112,6 +137,7 @@ def validate_msg(addr, msg):
         print(f"Validation Error: {e}")
         raise
 
+
 def stat_msg_valid(msg):
     try:
         if "height" not in msg or "hash" not in msg:
@@ -120,3 +146,21 @@ def stat_msg_valid(msg):
         return True
     except (ValueError, TypeError):
         return False
+
+
+"""
+## update this in send_gossip
+
+for each host, port in known_hosts:
+    try:
+        attempt sendto for this host and port
+        if successful, break the loop
+    except error:
+        log failure for this host
+        continue to the next host
+
+if all attempts failed:
+    handle the failure case
+
+also create a peer field to keep track of the successful one and then use that as priority
+"""
