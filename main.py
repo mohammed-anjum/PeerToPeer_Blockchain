8db72d9749cfaf3c26cff3c24a2a1905d9805859
vijdan-anjum <m-vijdan@hotmail.com>
2024-12-05 21:05:02 -0600

    calling send_get_block in main

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ def main():
     event_q.add_event(time.time() + 1, my_peer.send_gossip, None, 30)  # send_gossip
     # event_q.add_event(time.time() + 33, my_peer.check_gossipers, None, 30) # debug
     event_q.add_event(time.time() + 11, my_peer.send_stats, [my_peer.received_gossipers], 20)  # send_stat
-    event_q.add_event(time.time() + 40, my_peer.check_stats, None, 20)  # debug ~ 32 it should be ready
+    # event_q.add_event(time.time() + 40, my_peer.check_stats, None, 20)  # debug ~ 32 it should be ready
     event_q.add_event(time.time() + 45, my_peer.do_consensus, [my_peer.received_stats], 20)
+    event_q.add_event(time.time() + 66, my_peer.send_get_blocks, None, 10)
     ###
 
     while True:
